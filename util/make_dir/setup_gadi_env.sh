9e6f6c42417c9b3ae926a53c5354a2a180368165
Ryan Holmes <holmes.ryan.m@gmail.com>
2022-02-11 14:43:28 +1100

    Add Scott's make.inc and setup_gadi_env.sh.

---
+++ util/make_dir/setup_gadi_env.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Setup environment for CROCO/WRF/XIOS/OASIS on Gadi:
+module purge
+
+# New compilers from Chris Bladwell's XIOS-2.5 setup:
+module use ~access/modules
+
+module load intel-compiler/2020.2.254
+module load openmpi/4.0.2
+module load netcdf/4.8.0p
+module load hdf5/1.10.7p
+
+# The following are apparently required for XIOS-2.5:
+module load fcm
+module load drhook/1.1_ompi.4.0.2
+module load grib-api
+module load libpng
+module load openjpeg
+module load zlib
+
+# Previous setup that worked for everything except XIOS:
+# module load intel-compiler/2019.5.281
+# module load netcdf/4.7.1
+# module load openmpi/4.0.2
+# module load nco
+
+# Old old using access oasis3 modules: 
+# module use ~access/modules
+# module load oasis3-mct-local/ompi.4.0.2
+
