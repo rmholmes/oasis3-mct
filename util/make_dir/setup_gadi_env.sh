165942ac2fe74ecf25e92c2131764af2bc16f040
Ryan Holmes <holmes.ryan.m@gmail.com>
2022-02-12 11:28:44 +1100

    Change back to former intel compiler for WRF compile.

--- util/make_dir/setup_gadi_env.sh
+++ util/make_dir/setup_gadi_env.sh
@@ -5,7 +5,7 @@ module purge
 # New compilers from Chris Bladwell's XIOS-2.5 setup:
 module use ~access/modules
 
-module load intel-compiler/2020.2.254
+module load intel-compiler/2019.5.281
 module load openmpi/4.0.2
 module load netcdf/4.8.0p
 module load hdf5/1.10.7p
@@ -18,13 +18,23 @@ module load libpng
 module load openjpeg
 module load zlib
 
+# The following is WRF specific:
+module load pbs
+module load dot
+module load ncl/6.6.2
+
+export JASPERINC=/usr/include
+export JASPERLIB=/usr/lib64
+
+
 # Previous setup that worked for everything except XIOS:
 # module load intel-compiler/2019.5.281
 # module load netcdf/4.7.1
 # module load openmpi/4.0.2
-# module load nco
 
 # Old old using access oasis3 modules: 
 # module use ~access/modules
 # module load oasis3-mct-local/ompi.4.0.2
+
+module load nco
 
